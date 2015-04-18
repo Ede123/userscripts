470d2ad6ea26f1f08534c0f92a9d20e7aea47e79
Eduard Braun <Eduard.Braun2@gmx.de>
2015-04-18 22:57:33 +0200

    AMO Editors: Increase width of file viewer to prevent line breaking when possible

--- AMO_Editors.user.js
+++ AMO_Editors.user.js
@@ -1,18 +1,26 @@
 // ==UserScript==
 // @name        AMO Editors
 // @namespace   https://github.com/Ede123/userscripts
-// @version     0.9
+// @version     0.91
 // @description Automates some things for AMO editors
 // @author      Eduard Braun <eduard.braun2@gmx.de>
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include     https://addons.mozilla.org/*/editors/review/*
-// @grant       none
+// @include     https://addons.mozilla.org/*/firefox/files/browse/*
+// @include     https://addons.mozilla.org/*/firefox/files/compare/*
+// @grant       GM_addStyle
 // @require     https://raw.githubusercontent.com/darcyclarke/Detect.js/master/detect.min.js
+// @run-at      document-start
 // ==/UserScript==
 
-var inputOS = document.getElementById("id_operating_systems");
-var inputApp = document.getElementById("id_applications");
+GM_addStyle(".file-viewer .section { width: unset; max-width: 95%;}" +
+            ".file-viewer .syntaxhighlighter .line {max-width: unset;}");
 
-var ua = detect.parse(navigator.userAgent);
-inputOS.value = ua.os.name;
-inputApp.value = ua.browser.family + " " + ua.browser.major + "." + ua.browser.minor; 
+addEventListener('DOMContentLoaded', function() {
+	var inputOS = document.getElementById("id_operating_systems");
+	var inputApp = document.getElementById("id_applications");
+
+	var ua = detect.parse(navigator.userAgent);
+	inputOS.value = ua.os.name;
+	inputApp.value = ua.browser.family + " " + ua.browser.major + "." + ua.browser.minor;
+}, false);
