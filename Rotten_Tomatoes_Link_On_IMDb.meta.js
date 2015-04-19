06f8bb970ea3e798d34c3e45073f7ac15946117c
Eduard Braun <Eduard.Braun2@gmx.de>
2015-04-19 17:58:47 +0200

    Rotten Tomatoes Link On IMDb: Use RT API to get movie alias from IMDb ID after alias search has been discontinued.

--- Rotten_Tomatoes_Link_On_IMDb.meta.js
+++ Rotten_Tomatoes_Link_On_IMDb.meta.js
@@ -1,11 +1,12 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name        Rotten Tomatoes Link On IMDb
 // @namespace   https://github.com/Ede123/userscripts
-// @version     0.94
+// @version     0.96
 // @description Adds a direct link to the corresponding Rotten Tomatoes movie description page for every IMDb movie
 // @icon        https://raw.githubusercontent.com/Ede123/userscripts/master/icons/Rotten_Tomatoes.png
 // @author      Eduard Braun <eduard.braun2@gmx.de>
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @include     http://www.imdb.com/title/*
-// @grant       none
+// @include     http://www.imdb.com/title/tt*
+// @noframes
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
--- Rotten_Tomatoes_Link_On_IMDb.user.js
+++ Rotten_Tomatoes_Link_On_IMDb.user.js
@@ -1,35 +1,56 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name        Rotten Tomatoes Link On IMDb
 // @namespace   https://github.com/Ede123/userscripts
-// @version     0.94
+// @version     0.96
 // @description Adds a direct link to the corresponding Rotten Tomatoes movie description page for every IMDb movie
 // @icon        https://raw.githubusercontent.com/Ede123/userscripts/master/icons/Rotten_Tomatoes.png
 // @author      Eduard Braun <eduard.braun2@gmx.de>
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @include     http://www.imdb.com/title/*
-// @grant       none
+// @include     http://www.imdb.com/title/tt*
+// @noframes
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
 // get IMDb movie ID
 var IMDbID_RegEx = /\/title\/tt(\d{7})\//;
 var IMDbID = IMDbID_RegEx.exec(window.location.href)[1];
-//alert(IMDbID);
 
 
-// create link element to Rotten Tomatoes
-RT_link = "http://www.rottentomatoes.com/alias?type=imdbid&s=" + IMDbID;
-RT_icon = "http://www.rottentomatoes.com/favicon.ico";
+// function to add the Rotten Tomatoes button
+var addButton = function(link) {
+	// create link element to Rotten Tomatoes
+	var RT_link = link;
+	var RT_icon = "http://www.rottentomatoes.com/favicon.ico";
 
-var RT = document.createElement('span');
-RT.style.cssFloat = "right";
-RT.innerHTML = "<a target='_blank' href='" + RT_link + "'>" +
-                   "<img src='" + RT_icon + "' width='16' height='16' style='vertical-align:bottom'/>" +
-               "</a>";
+	var RT = document.createElement('span');
+	RT.style.cssFloat = "right";
+	RT.innerHTML = "<a target='_blank' href='" + RT_link + "'>" +
+	                   "<img src='" + RT_icon + "' width='16' height='16' style='vertical-align:bottom'/>" +
+	               "</a>";
 
-//add link to IMDb movie page
-var overview = document.getElementById("overview-top");
-if (overview) {
-    var star_box = overview.getElementsByClassName("star-box")[0];
-    var star_box_rating_widget = star_box.getElementsByClassName("star-box-rating-widget")[0];
-    star_box_rating_widget.appendChild(RT);
-}
+	//add link to IMDb movie page
+	var overview = document.getElementById("overview-top");
+	if (overview) {
+		var star_box = overview.getElementsByClassName("star-box")[0];
+		var star_box_rating_widget = star_box.getElementsByClassName("star-box-rating-widget")[0];
+		star_box_rating_widget.appendChild(RT);
+	}
+};
+
+
+// get Rotten Tomatoes movie alias from Rotten Tomatoes API
+GM_xmlhttpRequest({
+	method: "GET",
+	url: "http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?" +
+	     "type=imdb" + "&id=" + IMDbID + atob("JmFwaWtleT1kYWdxZGdod2FxM2UzbXh5cnA3a21tajU="),
+	onload: function(response) {
+		var json = JSON.parse(response.responseText);
+		if (json && json.id) {
+			var link = "http://www.rottentomatoes.com/m/" + json.id;
+			addButton(link);
+		}
+		else if (json && json.error) {
+			console.log("Error: " + json.error);
+		}
+	}
+});
