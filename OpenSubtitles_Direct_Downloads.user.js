705f1b559a683f9e819624074c4e8af6fb9e552c
Eduard Braun <Eduard.Braun2@gmx.de>
2015-08-25 19:42:50 +0200

    OpenSubtitles Direct Downloads: Fix for removed "rel" attribute

--- OpenSubtitles_Direct_Downloads.user.js
+++ OpenSubtitles_Direct_Downloads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        OpenSubtitles Direct Downloads
 // @namespace   https://github.com/Ede123/userscripts
-// @version     1.0.1
+// @version     1.0.2
 // @description Creates direct download links for subtitles on opensubtitles.org
 // @icon        https://raw.githubusercontent.com/Ede123/userscripts/master/icons/OpenSubtitles.png
 // @author      Eduard Braun <eduard.braun2@gmx.de>
@@ -17,9 +17,16 @@ GM_addStyle('#checkbox1,#checkbox2{display:none}');
 
 // modify button only after DOM is ready
 addEventListener('DOMContentLoaded', function() {
+	// check for download button on page
+	var downloadButton = document.getElementById('bt-dwl') || document.getElementById('bt-dwl-bt');
+	if(!downloadButton) return;
+
 	// create direct link avoiding advert page for "Open Subtitles MKV Player"
-	downloadButton = document.getElementById('bt-dwl') || document.getElementById('bt-dwl-bt');
-	downloadButton.href = downloadButton.rel.replace('subtitleserve','download');
+	var location = document.location.href;
+	var re = /subtitles\/(\d*)/i;
+	var subtitleID = location.match(re)[1];
+	downloadButton.href = "http://dl.opensubtitles.org/download/sub/" + subtitleID;
+	downloadButton.removeAttribute("onclick");
 	
 	// remove event listeners from the download button (by cloning and replacing it)
 	// to prevent any unwanted behavior
