6b2cd11a9feea9a3037de84373ad2604a5e4ecfe
Eduard Braun <Eduard.Braun2@gmx.de>
2016-02-14 03:31:51 +0100

    Rotten Tomatoes Link On IMDb: Adapt to latest IMDb UI changes

--- Rotten_Tomatoes_Link_On_IMDb.user.js
+++ Rotten_Tomatoes_Link_On_IMDb.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name        Rotten Tomatoes Link On IMDb
 // @namespace   https://github.com/Ede123/userscripts
-// @version     0.96
+// @version     1.0
 // @description Adds a direct link to the corresponding Rotten Tomatoes movie description page for every IMDb movie
 // @icon        https://raw.githubusercontent.com/Ede123/userscripts/master/icons/Rotten_Tomatoes.png
 // @author      Eduard Braun <eduard.braun2@gmx.de>
@@ -18,22 +18,29 @@ var IMDbID = IMDbID_RegEx.exec(window.location.href)[1];
 
 // function to add the Rotten Tomatoes button
 var addButton = function(link) {
-	// create link element to Rotten Tomatoes
-	var RT_link = link;
-	var RT_icon = "http://www.rottentomatoes.com/favicon.ico";
+	// icon
+	var RT_icon = document.createElement('img');
+	RT_icon.src = "http://www.rottentomatoes.com/favicon.ico";
+	RT_icon.width = RT_icon.height = 16;
+	RT_icon.style.verticalAlign = "bottom";
 
-	var RT = document.createElement('span');
-	RT.style.cssFloat = "right";
-	RT.innerHTML = "<a target='_blank' href='" + RT_link + "'>" +
-	                   "<img src='" + RT_icon + "' width='16' height='16' style='vertical-align:bottom'/>" +
-	               "</a>";
+	// link
+	var RT_link = document.createElement('a');
+	RT_link.target = "_blank";
+	RT_link.href = link;
+	
+	RT_link.appendChild(RT_icon);
+	
+	// spacer
+	var spacer = document.createElement('span');
+	spacer.classList.add("ghost");
+	spacer.textContent = "|";
 
 	//add link to IMDb movie page
-	var overview = document.getElementById("overview-top");
-	if (overview) {
-		var star_box = overview.getElementsByClassName("star-box")[0];
-		var star_box_rating_widget = star_box.getElementsByClassName("star-box-rating-widget")[0];
-		star_box_rating_widget.appendChild(RT);
+	var subtext = document.getElementsByClassName("subtext")[0];
+	if (subtext) {
+		subtext.appendChild(spacer);
+		subtext.appendChild(RT_link);
 	}
 };
 
