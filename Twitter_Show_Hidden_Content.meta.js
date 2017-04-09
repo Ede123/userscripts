d04f8647bf9dcb958ec2065358f25c5d516525ef
Eduard Braun <Eduard.Braun2@gmx.de>
2017-04-09 02:40:08 +0200

    Twitter Show Hidden Content: Update to work with current version of Twitter

--- Twitter_Show_Hidden_Content.meta.js
+++ Twitter_Show_Hidden_Content.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Twitter Show Hidden Content
 // @namespace   https://github.com/Ede123/userscripts
-// @version     1.0.2
+// @version     1.0.3
 // @description Removes the "sensitive material" warning on Twitter and unhides the content
 // @icon        https://raw.githubusercontent.com/Ede123/userscripts/master/icons/Twitter.png
 // @author      Eduard Braun <eduard.braun2@gmx.de>
